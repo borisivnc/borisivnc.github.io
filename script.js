f873915348c06b31dfcf910aed8082d84233b371
Boris IVANOVIC <ivanovic@d17-l428-pc-12.ipa.iisg.agh.edu.pl>
2018-10-19 09:33:01 +0200

    small changes - lab

---
+++ script.js
@@ -0,0 +1,8 @@
+let image = document.createElement("img");
+
+image.src = "image.jpeg";
+image.className = "img-thumbmail";
+
+var divCont = document.getElementById("bodyContainer");
+
+divCont.appendChild(image);
