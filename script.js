3dbd9944e5f598bcd4cb2738cbd23308e20db9e3
borisivnc <32333852+borisivnc@users.noreply.github.com>
2018-10-30 00:49:57 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let image = document.createElement("img");
-
-image.src = "image.jpeg";
-image.className = "img-thumbmail";
-
-var divCont = document.getElementById("bodyContainer");
-
-divCont.appendChild(image);
